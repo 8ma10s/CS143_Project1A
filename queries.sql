8d135f87d7a62241a467ade59dca5b97803f68ad
Ben Lawson <blinternetgeek@gmail.com>
2017-04-12 14:01:48 -0700

    improved query 1, wrote query 3

--- queries.sql
+++ queries.sql
@@ -1,1 +1,15 @@
-SELECT CONCAT(Actor.first, ' ', Actor.last) FROM Actor JOIN MovieActor ON Actor.id = MovieActor.aid JOIN Movie ON MovieActor.mid = Movie.id WHERE Movie.title = 'Die Another Day';
+-- names of all the actors in the movie 'Die Another Day'
+SELECT CONCAT(Actor.first, ' ', Actor.last) FROM Actor, MovieActor, Movie
+WHERE Actor.id = MovieActor.aid
+AND MovieActor.mid = Movie.id
+AND Movie.title = 'Die Another Day';
+
+-- count of all the actors who acted in multiple movies
+
+
+--  title of movies that sell more than 1,000,000 tickets
+SELECT Movie.title FROM Movie, Sales
+WHERE Movie.id = Sales.mid
+AND Sales.ticketsSold > 1000000;
+
+
