a43564590c46bbab27110c56cc13255d7ed5f31b
Ben Lawson <blinternetgeek@gmail.com>
2017-04-15 13:38:11 -0700

    finished queries

--- queries.sql
+++ queries.sql
@@ -5,7 +5,10 @@ AND MovieActor.mid = Movie.id
 AND Movie.title = 'Die Another Day';
 
 -- count of all the actors who acted in multiple movies
-
+SELECT COUNT(aid) as ActorsInMultipleMovies
+FROM MovieActor
+GROUP BY aid, mid
+HAVING COUNT(aid) > 1;
 
 --  title of movies that sell more than 1,000,000 tickets
 SELECT Movie.title FROM Movie, Sales
@@ -15,9 +18,21 @@ AND Sales.ticketsSold > 1000000;
 -- OUR OWN QUERIES:
 
 -- Average imdb rating of movies directed by James Cameron
-SELECT avg(MovieRating.imdb) as AverageDirectorRating FROM MovieRating, MovieDirector, Director
+SELECT avg(MovieRating.imdb) as AverageDirectorRating
+FROM MovieRating, MovieDirector, Director
 WHERE MovieRating.mid = MovieDirector.mid
 AND MovieDirector.did = Director.id
 AND Director.first = 'James' AND Director.last = 'Cameron';
 
+-- The title of the movie with the most actors in 2002, and the number of actors in that movie
+SELECT Movie.title, COUNT(mid) as NumberOfActors
+FROM MovieActor, Movie
+WHERE MovieActor.mid = Movie.id
+AND Movie.year = 2002
+GROUP BY Movie.title
+HAVING COUNT(Movie.title) >= ALL (SELECT COUNT(MA.mid)
+       FROM MovieActor as MA, Movie as M
+       WHERE MA.mid = M.id
+       AND M.year = 2002
+       GROUP BY MA.mid);
 
